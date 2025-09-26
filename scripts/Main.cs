05a0c94995810543d70cce68bef64c70f4d7cc19
rsudul <alwalt93@gmail.com>
2025-09-26 23:17:57 +0200

    chore: bootstrap Godot 4 (.NET) project with C# entry scene

---
+++ scripts/Main.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+public partial class Main : Node2D
+{
+    public override void _Ready()
+    {
+        GD.Print("Main ready!");
+    }
+
+    public override void _Process(double delta)
+    {
+
+    }
+}
